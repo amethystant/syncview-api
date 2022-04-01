9d9bbabd21ff264c85408cd6d22e41884ad107f2
Karel Patlejch <patlejch.karel@gmail.com>
2022-04-01 12:09:57 +0100

    fixed endpoints not accepting uppercased session codes

--- uc/find-session.js
+++ uc/find-session.js
@@ -4,7 +4,8 @@ const {SESSION_DURATION_MAX} = require('../constants')
 module.exports = (sessionDb) => {
 
     return (sessionCode) => {
-        let sessionExists = sessionCode && sessionCode in sessionDb
+        sessionCode = sessionCode ? sessionCode.toLowerCase() : null
+        const sessionExists = sessionCode && sessionCode in sessionDb
         if (sessionExists && Date.now() < sessionDb[sessionCode].startTs + SESSION_DURATION_MAX) {
             return sessionDb[sessionCode.toLowerCase()]
         }
