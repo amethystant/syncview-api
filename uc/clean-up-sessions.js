5f9b54df804714db43135e879423721ed1400a7f
Karel Patlejch <patlejch.karel@gmail.com>
2022-04-15 15:02:16 +0100

    fixed session cleanup crashing the app if a session has been deleted before expiration

--- uc/clean-up-sessions.js
+++ uc/clean-up-sessions.js
@@ -5,6 +5,10 @@ module.exports = (sessionDurationMax, sessionsDb, sessionCleanupRepo, endSession
 
         for (const code of codes) {
             let session = sessionsDb[code]
+            if (!session) {
+                continue
+            }
+
             if (session.startTs + sessionDurationMax > ts) {
                 throw new Error('Session not expired but scheduled for cleanup!')
             }
